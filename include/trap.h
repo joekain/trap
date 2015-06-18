1146ba8c1b31065fd648c8df54bec4bfd3516889
Joseph Kain <joekain@gmail.com>
2015-06-18 00:54:48 -0700

    Document typedefs

--- include/trap.h
+++ include/trap.h
@@ -12,8 +12,33 @@
  * @see http://system.joekain.com/debugger/
  */
 
+/**
+ * @brief A handle to an inferior process under the control of the
+ *   Trap debugger.
+ *
+ * A `trap_inferior_t` is used to refer to an inferior when making
+ * Trap API calls.
+ */
 typedef int trap_inferior_t;
+
+/**
+ * @brief A handle to a breakpoint set in a specific inferior process
+ *   under the control of the Trap debugger.
+ *
+ * A `trap_breakppint_t` handle is used to refer to a breakpoint in
+ * Trap API calls.
+ * 
+ * A `trap_breakpoint_t` handle is only meaningful given a specific
+ * inferior.  That is, the handle must always be used in conjunction
+ * with a `trap_inferior_t`.
+ */
 typedef int trap_breakpoint_t;
+
+/**
+ * @brief A function pointer used as a callback.
+ * 
+ * @see trap_breakpoint_set_callback
+ */
 typedef void (*trap_breakpoint_callback_t)(trap_inferior_t, trap_breakpoint_t);
 
 /**
