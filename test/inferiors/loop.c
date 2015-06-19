ce945d3d79744dca6cc9ec1e69ee47215fd6cfda
Joseph Kain <joekain@gmail.com>
2015-06-19 04:51:36 -0700

    test_multi_breakpoint
    A test for breakpoints that are hit more than once.  Currently disabled.

---
+++ test/inferiors/loop.c
@@ -0,0 +1,13 @@
+int foo()
+{
+  return 5;
+}
+
+int main()
+{
+  for (int i = 0; i < 5; i++) {
+    foo();
+  }
+
+  return 0;
+}
---
+++ test/test_multi_breakpoint.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <trap.h>
+#include <unistd.h>
+
+int g_breakpoint_count = 0;
+trap_inferior_t g_inferior = 0;
+trap_breakpoint_t g_bp = 0;
+
+void breakpoint_callback(trap_inferior_t inferior, trap_breakpoint_t bp)
+{
+  assert(inferior == g_inferior);
+  assert(bp == g_bp);
+
+  g_breakpoint_count++;
+}
+
+int main()
+{
+  char *argv[1] = { 0 };
+
+  trap_breakpoint_set_callback(breakpoint_callback);
+  g_inferior = trap_inferior_exec("./inferiors/loop", argv);
+  g_bp = trap_inferior_set_breakpoint(g_inferior, (char *)0x000000000040076d);
+  trap_inferior_continue(g_inferior);
+
+  assert(g_breakpoint_count == 5);
+
+  return 0;
+}
