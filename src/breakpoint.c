8438c8c066803638b2fb982c60b14ef8af3b8d45
Joseph Kain <joekain@gmail.com>
2015-06-19 00:28:00 -0700

    Use Read / Modify / Write when inserting INT3

--- src/breakpoint.c
+++ src/breakpoint.c
@@ -2,12 +2,10 @@
 #include <sys/ptrace.h>
 #include <unistd.h>
 #include <stdio.h>
-
-static const void *ignored_ptr;
-static const void *no_continue_signal = 0;
+#include <stdint.h>
 
 static trap_breakpoint_callback_t g_callback;
-static unsigned char g_original_breakpoint_byte;
+static uintptr_t g_original_breakpoint_word;
 
 void trap_breakpoint_set_callback(trap_breakpoint_callback_t callback)
 {
@@ -17,13 +15,23 @@ void trap_breakpoint_set_callback(trap_breakpoint_callback_t callback)
 trap_breakpoint_t trap_inferior_set_breakpoint(trap_inferior_t inferior,
                                                char *location)
 {
-  const void *target_address = (void *)0x000000000040079d;
-  const void *int3_opcode = (void *)0xCC;
+  const uintptr_t target_address = (void *)0x000000000040079d;
+  const uintptr_t int3_opcode = 0xCC;
   pid_t inferior_pid = inferior;
+  uintptr_t modified_word;
 
-  ptrace(PTRACE_PEEKTEXT, inferior_pid, target_address,
-	 &g_original_breakpoint_byte);
-  ptrace(PTRACE_POKETEXT, inferior_pid, target_address, int3_opcode);
+  uintptr_t aligned_address = target_address & ~(0x7UL);
+  uintptr_t target_offset = target_address - aligned_address;
+
+
+  g_original_breakpoint_word = ptrace(PTRACE_PEEKTEXT, inferior_pid,
+				      aligned_address, 0);
+  printf("Original word: 0x%016lx\n", g_original_breakpoint_word);
+  modified_word = g_original_breakpoint_word;
+  modified_word &= ~(0xFFUL << (target_offset * 8));
+  modified_word |= int3_opcode << (target_offset * 8);
+  printf("Original word: 0x%016lx\n", modified_word);
+  ptrace(PTRACE_POKETEXT, inferior_pid, aligned_address, modified_word);
 
   return 0;
 }
@@ -34,7 +42,7 @@ void breakpoint_trigger_callback(trap_inferior_t inferior,
   (*g_callback)(inferior, handle);
 }
 
-void breakpoint_resolve(trap_inferior_t inferior)
+trap_breakpoint_t breakpoint_resolve(trap_inferior_t inferior)
 {
   return 0;
 }
@@ -42,9 +50,8 @@ void breakpoint_resolve(trap_inferior_t inferior)
 void breakpoint_remove(trap_inferior_t inferior, trap_breakpoint_t handle)
 {
   const void *target_address = (void *)0x000000000040079d;
-  const void *int3_opcode = (void *)0xCC;
   pid_t inferior_pid = inferior;
 
   ptrace(PTRACE_POKETEXT, inferior_pid, target_address,
-	 g_original_breakpoint_byte);
+	 g_original_breakpoint_word);
 }
