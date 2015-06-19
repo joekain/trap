1eae244bd13f7a920daa57097609014e4c0cff48
Joseph Kain <joekain@gmail.com>
2015-06-19 09:10:31 -0700

    Fix up naming conventions

--- src/inferior.h
+++ src/inferior.h
@@ -1,12 +1,18 @@
+#if !defined(_TRAP_INFERIOR_H)
+#define _TRAP_INFERIOR_H
+
 #include <sys/types.h>
 #include <unistd.h>
 
-enum InferiorState {
+enum inferior_state_t {
   INFERIOR_RUNNING,
   INFERIOR_STOPPED,
 };
 
-struct Inferior {
+struct Inferior_t {
   pid_t pid;
-  enum InferiorState state;
+  enum inferior_state_t state;
 };
+typedef struct inferior_t inferior_t;
+
+#endif /* _TRAP_INFERIOR_H */
