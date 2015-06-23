60848350c96adc2279d3eacdf10ed4eda2c28ab3
Joseph Kain <joekain@gmail.com>
2015-06-23 13:27:38 -0700

    Tests for deleting breakpoints

---
+++ test/test_delete_breakpoint.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <trap.h>
+#include <unistd.h>
+
+int g_main_count = 0;
+
+trap_inferior_t g_inferior = 0;
+trap_breakpoint_t g_bp_main = 0;
+
+void breakpoint_callback(trap_inferior_t inferior, trap_breakpoint_t bp)
+{
+  assert(inferior == g_inferior);
+  g_main_count++;
+}
+
+int main()
+{
+  char *argv[1] = { 0 };
+
+  trap_breakpoint_set_callback(breakpoint_callback);
+  g_inferior = trap_inferior_exec("./inferiors/loop", argv);
+  g_bp_main = trap_inferior_set_breakpoint(g_inferior, (char *)0x0000000000400778);
+  trap_inferior_remove_breakpoint(g_inferior, g_bp_main);
+  trap_inferior_continue(g_inferior);
+
+  assert(g_main_count == 0);
+
+  return 0;
+}
---
+++ test/test_delete_breakpoint_in_callback.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <trap.h>
+#include <unistd.h>
+
+int g_foo_count = 0;
+
+trap_inferior_t g_inferior = 0;
+trap_breakpoint_t g_bp_foo = 0;
+
+void breakpoint_callback(trap_inferior_t inferior, trap_breakpoint_t bp)
+{
+  assert(inferior == g_inferior);
+  g_foo_count++;
+  trap_inferior_remove_breakpoint(g_inferior, bp);
+}
+
+int main()
+{
+  char *argv[1] = { 0 };
+
+  trap_breakpoint_set_callback(breakpoint_callback);
+  g_inferior = trap_inferior_exec("./inferiors/loop", argv);
+  g_bp_foo = trap_inferior_set_breakpoint(g_inferior, (char *)0x000000000040076d);
+  trap_inferior_continue(g_inferior);
+
+  assert(g_foo_count == 1);
+
+  return 0;
+}
